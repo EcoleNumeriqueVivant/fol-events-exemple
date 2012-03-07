b3fb43a1c32ae87d04c63bebf3c5ec980d7460c5
Clément Hussenot <chussenot@gmail.com>
2012-03-07 11:20:05 +0100

    fist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FolEvents::Application.config.secret_token = '263d2e4d80c7ee83a6452158364c0f0c22e4b5df890bcca9bf087d5b9e8b2865ae86ec9d73abb1c351ec8e72799a755fda44700448171e9c037960ac8293f6c8'
