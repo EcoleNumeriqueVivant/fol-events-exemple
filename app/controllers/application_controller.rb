1ec29fd11f1231c4407fefeb8db7e8e11c248f0a
Clement Hussenot <chussenot@gmail.com>
2012-04-18 23:04:09 +0200

    change MailChimp path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
    http_basic_authenticate_with name: "fol", password: "fol"
    
-   require File.expand_path(File.dirname(__FILE__)) + "/../models/MailChimp.rb"
+   # require File.expand_path(File.dirname(__FILE__)) + "/../models/MailChimp.rb"
    
    def add_to_list
      puts params.inspect
-     MailChimp.subscribe params[:mail]
+     # MailChimp.subscribe params[:mail]
      render :nothing => true, :status => 200
    end   
    
