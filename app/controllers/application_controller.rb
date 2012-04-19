238fa76aecea2fad59113ec00c0241335d9c7358
Clement Hussenot <chussenot@gmail.com>
2012-04-19 23:28:34 +0200

    try to fix user access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
    
+   helper_method :current_user
+   # before_filter :check_user
+   
    # require File.expand_path(File.dirname(__FILE__)) + 
    # require "./MailChimp.rb"
    
@@ -14,15 +17,23 @@ class ApplicationController < ActionController::Base
    end 
    
    def dashboard 
-   end   
+   end
    
-   helper_method :current_user
+   
+   def check_user
+       if current_user.blank?
+         respond_to do |format|
+           format.html do
+              redirect_to root_path 
+           end
+         end
+       end 
+   end
 
    private
 
    def current_user
      @current_user ||= User.find(session[:user_id]) if session[:user_id]
    end
-   
-   
+       
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 class SessionsController < ApplicationController
   
   def new
@@ -7,16 +9,16 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to :controller => "application", :action => "dashboard", :notice => "Logged in!"
     else
-      flash.now.alert = "Invalid email or password"
-      render "new"
+      flash.now.alert = "email ou mot de passe invalide"
+      render [:default, :root]
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to root_url, :notice => "Vous êtes déconnecté"
   end
 
 end
