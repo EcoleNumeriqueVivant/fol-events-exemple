b5472b0ce0371dd06455208f541a1506bacf80ce
Clement Hussenot <chussenot@gmail.com>
2012-04-21 14:28:53 +0200

    introduce a rating system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,11 @@ class UsersController < ApplicationController
   # PUT /backend/users/1
   # PUT /backend/users/1.json
   def update
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
       if @user.update_attributes(params[:user])
         format.html { redirect_to :back, notice: 'Vos informations ont été mis à jour.' }
       else
         format.html { render controller: "account", action: "index" }
       end
-    end
   end
-
 end
