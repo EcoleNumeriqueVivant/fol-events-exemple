501af03351153e25591b35a880e610ec0100ff9f
chussenot <chussenot@gmail.com>
2014-07-07 13:34:32 +0200

    Add bower to manage components

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,7 @@ module FolEvents
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    I18n.enforce_available_locales = false
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -44,5 +45,8 @@ module FolEvents
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Add Bower components
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
   end
 end
