41f1b2388a75fa88b4b099a13885bb14d76c28ce
chussenot <chussenot@gmail.com>
2014-08-06 23:46:18 +0200

    Fix act_as_rated for rails 4

--- db/schema.rb
+++ db/schema.rb
@@ -9,32 +9,35 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120424084626) do
+ActiveRecord::Schema.define(version: 20140806204428) do
 
-  create_table "addresses", :force => true do |t|
-    t.string  "line1",            :default => ""
-    t.string  "line2",            :default => ""
-    t.string  "city",             :default => ""
-    t.string  "state",            :default => ""
-    t.string  "country",          :default => ""
-    t.string  "zip",              :default => ""
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "addresses", force: true do |t|
+    t.string  "line1",            default: ""
+    t.string  "line2",            default: ""
+    t.string  "city",             default: ""
+    t.string  "state",            default: ""
+    t.string  "country",          default: ""
+    t.string  "zip",              default: ""
     t.integer "addressable_id"
-    t.string  "addressable_type", :default => ""
+    t.string  "addressable_type", default: ""
     t.float   "latitude"
     t.float   "longitude"
   end
 
-  add_index "addresses", ["addressable_type", "addressable_id"], :name => "index_addresses_on_addressable_type_and_addressable_id", :unique => true
+  add_index "addresses", ["addressable_type", "addressable_id"], name: "index_addresses_on_addressable_type_and_addressable_id", unique: true, using: :btree
 
-  create_table "comments", :force => true do |t|
-    t.integer  "commentable_id",   :default => 0
-    t.string   "commentable_type", :default => ""
-    t.string   "title",            :default => ""
-    t.text     "body",             :default => ""
-    t.string   "subject",          :default => ""
-    t.integer  "user_id",          :default => 0,  :null => false
+  create_table "comments", force: true do |t|
+    t.integer  "commentable_id",   default: 0
+    t.string   "commentable_type", default: ""
+    t.string   "title",            default: ""
+    t.text     "body",             default: ""
+    t.string   "subject",          default: ""
+    t.integer  "user_id",          default: 0,  null: false
     t.integer  "parent_id"
     t.integer  "lft"
     t.integer  "rgt"
@@ -42,10 +45,10 @@ ActiveRecord::Schema.define(:version => 20120424084626) do
     t.datetime "updated_at"
   end
 
-  add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
-  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+  add_index "comments", ["commentable_id"], name: "index_comments_on_commentable_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
-  create_table "events", :force => true do |t|
+  create_table "events", force: true do |t|
     t.string   "name"
     t.date     "begin_date"
     t.text     "description"
@@ -62,48 +65,58 @@ ActiveRecord::Schema.define(:version => 20120424084626) do
     t.text     "infos_extra"
     t.integer  "rating_count"
     t.decimal  "rating_total"
-    t.decimal  "rating_avg",          :precision => 10, :scale => 2
+    t.decimal  "rating_avg",          precision: 10, scale: 2
     t.string   "publish_state"
   end
 
-  create_table "ratings", :force => true do |t|
+  create_table "ratings", force: true do |t|
     t.integer "rater_id"
     t.integer "rated_id"
     t.string  "rated_type"
     t.decimal "rating"
   end
 
-  add_index "ratings", ["rated_type", "rated_id"], :name => "index_ratings_on_rated_type_and_rated_id"
-  add_index "ratings", ["rater_id"], :name => "index_ratings_on_rater_id"
+  add_index "ratings", ["rated_type", "rated_id"], name: "index_ratings_on_rated_type_and_rated_id", using: :btree
+  add_index "ratings", ["rater_id"], name: "index_ratings_on_rater_id", using: :btree
 
-  create_table "taggings", :force => true do |t|
+  create_table "taggings", force: true do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
     t.string   "taggable_type"
     t.integer  "tagger_id"
     t.string   "tagger_type"
-    t.string   "context",       :limit => 128
+    t.string   "context",       limit: 128
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
-  create_table "tags", :force => true do |t|
+  create_table "tags", force: true do |t|
     t.string  "name"
     t.string  "context"
     t.integer "position"
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
     t.string   "first_name"
     t.string   "last_name"
     t.string   "phone"
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
