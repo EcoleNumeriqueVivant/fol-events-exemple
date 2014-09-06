ad05a276b6e310054f49adecacc26dfea3493ac2
Clément Hussenot <chussenot@gmail.com>
2014-09-06 23:11:27 +0200

    Remove /test

--- test/unit/helpers/backend/comments_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Backend::CommentsHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/doc_export_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class DocExportHelperTest < ActionView::TestCase
-end
