210837a23dc7f4e94fca097cbe12001323bfc341
Clément Hussenot <chussenot@gmail.com>
2014-09-06 22:24:38 +0200

    Configuring routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,18 @@ FolEvents::Application.routes.draw do
   # TODO
   # take a look at this gem
   # https://github.com/pluff/grape-devise/blob/master/lib/grape/devise/endpoints/authentication.rb
-  devise_for :users, :skip => [:sessions]
-  as :user do
-    get 'users/sign' => 'devise/sessions#new', :as => :new_user_session
-    post 'users/sign' => 'devise/sessions#create', :as => :user_session
-    delete 'users/sign' => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
+  devise_for :users, path: "api/auth", path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    password: 'secret',
+    confirmation: 'verification',
+    unlock: 'unblock',
+    registration: 'register',
+    sign_up: 'cmon_let_me_in'
+  }
 
   mount FOL::API::Root => 'api'
 
+  get 'admin' => 'admin#index', :as => :admin_index
   root to: "home#index"
 end
