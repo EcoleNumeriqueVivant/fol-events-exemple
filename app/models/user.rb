6dc9dbd0a942bd97d36d4af0b8b8ebf02338c387
Clément Hussenot <chussenot@gmail.com>
2014-09-06 22:26:09 +0200

    Remove validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,9 +20,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :email
-  validates_uniqueness_of :email
-
   def name
     first_name + " " + last_name
   end
