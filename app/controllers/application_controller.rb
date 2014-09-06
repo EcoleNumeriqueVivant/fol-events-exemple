ddfc8b788b47c84b91eb5b7fdad89332464c4f55
Clément Hussenot <chussenot@gmail.com>
2014-09-06 22:25:48 +0200

    Remove forgery protection and add permetted params for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
+  end
+
 end
