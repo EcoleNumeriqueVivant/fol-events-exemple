005a31c32c722f43783fb93af3a845880752cfe6
Clément Hussenot <chussenot@gmail.com>
2014-09-06 22:49:06 +0200

    Update devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ FolEvents::Application.routes.draw do
   # take a look at this gem
   # https://github.com/pluff/grape-devise/blob/master/lib/grape/devise/endpoints/authentication.rb
   devise_for :users, path: "api/auth", path_names: {
-    sign_in: 'login',
-    sign_out: 'logout',
-    password: 'secret',
+    sign_in:      'login',
+    sign_out:     'logout',
+    password:     'secret',
     confirmation: 'verification',
-    unlock: 'unblock',
+    unlock:       'unlock',
     registration: 'register',
-    sign_up: 'cmon_let_me_in'
+    sign_up:      'sign_up'
   }
 
   mount FOL::API::Root => 'api'
