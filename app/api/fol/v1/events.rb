3e774856e2c8cd8a6dad12f3c39ef99449d80529
Clément Hussenot <chussenot@gmail.com>
2014-09-07 22:15:54 +0200

    WIP :: Event CRUD

--- app/api/fol/v1/events.rb
+++ app/api/fol/v1/events.rb
@@ -6,13 +6,43 @@ module FOL
         format :json
         version 'v1'
         resource :events do
+
           desc "Return list of all events"
           get do
-            Event.all
+            Event.all.as_json
           end
+
           desc "Return an event by :id"
           get ':id' do
-            Event.find(params[:id])
+            event = Event.find(params[:id])
+            error! {message:"Not Found"}  unless event
+            event.as_json
+          end
+
+          desc "Update an event by :id",
+           params: Hash[Event.attribute_names.map{|sym| [sym, {}]}].merge(
+            "id" => { description: "event id", required: true }
+           )
+          put ':id' do
+            event = Event.find(params[:id])
+            error! {message:"Not Found"} unless event
+            values = {}
+            event.update_attributes!(values)
+            event.as_json
+          end
+
+          desc "Delete an event by :id"
+          delete ':id' do
+            event = Event.find(params[:id])
+            error! {message:"Not Found"} unless event
+            event.destroy
+            event.as_json
+          end
+
+          desc "Create an event"
+          post do
+            event = Event.create!()
+            event.as_json
           end
         end
       end
